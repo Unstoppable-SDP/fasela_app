4bac0549d2a445b378ed91a261eacf54cf22ca01
MarwahBakoor <marwahbakoor@gmail.com>
2022-12-18 02:43:07 +0300

    feat: creat new plant

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,218 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // firebase
+  final _fireStore = FirebaseFirestore.instance;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // form variables
+  late String name;
+  late String type;
+  late String description;
+  late String age;
+  late TextEditingController nameTextController;
+  late TextEditingController typeTextController;
+  late TextEditingController descriptionTextController;
+  late TextEditingController ageTextController;
+
+  @override
+  void initState() {
+    super.initState();
+    nameTextController = TextEditingController();
+    typeTextController = TextEditingController();
+    descriptionTextController = TextEditingController();
+    ageTextController = TextEditingController();
   }
 
   @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    nameTextController.dispose();
+    typeTextController.dispose();
+    descriptionTextController.dispose();
+    ageTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
+        body: SingleChildScrollView(
+            child: Padding(
+      padding: const EdgeInsets.all(37.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 26),
+            child: Text(
+              'Add New Plant',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 48,
+                color: Colors.black,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Plant Name",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              )),
+          TextField(
+            controller: nameTextController,
+            onChanged: (value) {
+              name = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Name',
+              alignLabelWithHint: true,
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 18.0,
+          ),
+          const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Type",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              )),
+          TextField(
+            controller: typeTextController,
+            onChanged: (value) {
+              type = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Type',
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 18.0,
+          ),
+          const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Age",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              )),
+          TextField(
+            controller: ageTextController,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              age = value;
+            },
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.all(10),
+              hintText: 'Age',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 18.0,
+          ),
+          const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Description",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              )),
+          TextField(
+            controller: descriptionTextController,
+            onChanged: (value) {
+              description = value;
+            },
+            maxLines: 5,
+            decoration: const InputDecoration(
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              hintText: 'Description',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Center(
+                child: Material(
+              // elevation: 5.0,
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(30.0),
+
+              child: MaterialButton(
+                minWidth: 200.0,
+                height: 42.0,
+                child: const Text(
+                  'Create Plant',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  _fireStore.collection('Plants').add({
+                    'name': name,
+                    'type': type,
+                    'description': description,
+                    'age': age,
+                    'created': Timestamp.now(),
+                  });
+                  nameTextController.clear();
+                  typeTextController.clear();
+                  descriptionTextController.clear();
+                  ageTextController.clear();
+                },
+              ),
+            )),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    )));
   }
 }
