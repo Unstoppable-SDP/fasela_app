b0e92d3335c0126f49bcccd571e8cf2b05f14b8f
MarwahBakoor <marwahbakoor@gmail.com>
2022-12-22 21:47:35 +0300

    feat: plants list

---
+++ lib/plantsList.dart
@@ -0,0 +1,143 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class PlantList extends StatefulWidget {
+  PlantList({super.key});
+
+  @override
+  _PlantListState createState() => _PlantListState();
+}
+
+class _PlantListState extends State<PlantList> {
+  final _fireStore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Plants List'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            ///
+            stream: _fireStore
+                .collection('Plants')
+                .orderBy('created', descending: false)
+                .snapshots(),
+
+            ///flutter aysnc snapshot
+            builder: (context, snapshot) {
+              List<MessageBubble> todoWidgets = [];
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.lightBlueAccent,
+                  ),
+                );
+              }
+              final todoLists = snapshot.data!.docs;
+
+              for (var todoList in todoLists) {
+                final name = (todoList.data() as dynamic)['name'];
+
+                final age = (todoList.data() as dynamic)['age'];
+
+                final type = (todoList.data() as dynamic)['type'];
+
+                final time = (todoList.data() as dynamic)['time'];
+
+                final messageWidget = MessageBubble(
+                  name: '$name',
+                  age: '$age',
+                  type: '$type',
+                  time: '$time',
+                );
+
+                todoWidgets.add(messageWidget);
+              }
+
+              return Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(children: todoWidgets),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble(
+      {required this.name,
+      required this.age,
+      required this.time,
+      required this.type});
+  final String name;
+  final String age;
+  final String time;
+  final String type;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Text('$name',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 30.0, color: Colors.black)),
+            ],
+          ),
+          Material(
+              elevation: 5.0,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0),
+                  bottomLeft: Radius.circular(30.0)),
+              color: Colors.lightBlueAccent,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('$age',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('$type',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('$time',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          )),
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
