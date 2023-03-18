f63a83ee5a53981c5377d996d85894ca4a87b457
MarwahBakoor <marwahbakoor@gmail.com>
2023-03-18 12:25:41 +0300

    fix: fix the model not working

--- lib/DiseaseDetection.dart
+++ lib/DiseaseDetection.dart
@@ -14,15 +14,15 @@ class _DiseaseDetectionState extends State<DiseaseDetection> {
   late List _output;
   final picker = ImagePicker();
 
-  @override 
+  @override
   void initState() {
     super.initState();
-    loadModel().then((value){
+    loadModel().then((value) {
       setState(() {});
     });
   }
 
-  @override 
+  @override
   void dispose() {
     super.dispose();
     Tflite.close();
@@ -32,11 +32,11 @@ class _DiseaseDetectionState extends State<DiseaseDetection> {
     var output = await Tflite.runModelOnImage(
       path: image.path,
       numResults: 11,
-      threshold: 0.5,
-      imageMean: 127.5,
-      imageStd: 127.5,
+      threshold: 0,
+      imageMean: 0.0,
+      imageStd: 1.0,
     );
-
+    print(output);
     setState(() {
       _output = output!;
       _loading = false;
@@ -56,7 +56,7 @@ class _DiseaseDetectionState extends State<DiseaseDetection> {
 
     setState(() {
       if (image != null) {
-       _image = File(image.path);
+        _image = File(image.path);
       }
     });
 
@@ -71,7 +71,7 @@ class _DiseaseDetectionState extends State<DiseaseDetection> {
 
     setState(() {
       if (image != null) {
-       _image = File(image.path);
+        _image = File(image.path);
       }
     });
 
@@ -83,117 +83,125 @@ class _DiseaseDetectionState extends State<DiseaseDetection> {
   loader() {
     // here any widget would do
     return AlertDialog(
-        title: Text('Wait.. Loading data..'),
+      title: Text('Wait.. Loading data..'),
     );
-}
+  }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 216, 223, 246),
-        centerTitle: true,
-        title: Text(
-          'Disease Detection',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-            fontSize: 23,
-          ),
-        ),
-      ),
-      body: Container(
-        color: Color.fromARGB(255, 216, 223, 246),
-        padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
-        child: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(30),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Center(
-                  child: _loading == true 
-                  ? null
-                  : Container(
-                    child: Column(
-                      children: [
-                        Container(
-                          height: MediaQuery.of(context).size.width * 0.5,
-                          width: MediaQuery.of(context).size.width * 0.5,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30),
-                            child: Image.file(
-                              _image,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                        _output != null
-                          ? Text(
-                            'The toamto condition is ${_output[0]['label']}',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          )
-                          : Container(),
-                          Divider(
-                            height: 25,
-                            thickness: 1,
-                          ),
-                      ],
-                    ),
-                    ),
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 216, 223, 246),
+              centerTitle: true,
+              title: Text(
+                'Disease Detection',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 23,
                 ),
               ),
-              Container(
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: pickImage,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 200,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 17),
-                        decoration: BoxDecoration(color: Color.fromARGB(255, 216, 223, 246), borderRadius: BorderRadius.circular(15),),
-                        child: Text(
-                          'Take A Photo',
-                          style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+            body: Container(
+                color: Color.fromARGB(255, 216, 223, 246),
+                padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Center(
+                          child: _loading == true
+                              ? null
+                              : Container(
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        height:
+                                            MediaQuery.of(context).size.width *
+                                                0.5,
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.5,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          child: Image.file(
+                                            _image,
+                                            fit: BoxFit.fill,
+                                          ),
+                                        ),
+                                      ),
+                                      _output != null
+                                          ? Text(
+                                              'The toamto condition is  ${_output[0]['label']}',
+                                              style: const TextStyle(
+                                                color: Colors.blue,
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w400,
+                                              ),
+                                            )
+                                          : Container(),
+                                      Divider(
+                                        height: 25,
+                                        thickness: 1,
+                                      ),
+                                    ],
+                                  ),
+                                ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    GestureDetector(
-                      onTap: pickGalleryImage,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 200,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 17),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 216, 223, 246),
-                          borderRadius: BorderRadius.circular(15)
-                        ),
-                        child: Text(
-                          'Pick from gallery',
-                          style: TextStyle(color: Colors.black, fontSize: 16),
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                              onTap: pickImage,
+                              child: Container(
+                                width: MediaQuery.of(context).size.width - 200,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 17),
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 216, 223, 246),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Text(
+                                  'Take A Photo',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 16),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            GestureDetector(
+                              onTap: pickGalleryImage,
+                              child: Container(
+                                width: MediaQuery.of(context).size.width - 200,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 17),
+                                decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 216, 223, 246),
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Text(
+                                  'Pick from gallery',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        )
-      )
-    )
-    );
+                    ],
+                  ),
+                ))));
   }
 }
